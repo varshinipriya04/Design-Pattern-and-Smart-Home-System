8d0afa0b060242d50e10fbaf06a16158c79de0d1
varshinipriya04 <varshini.sinju@gmail.com>
2025-09-27 13:05:20 +0530

    Create DeviceFactory.java

---
+++ Smart Home/factory/DeviceFactory.java
@@ -0,0 +1,18 @@
+package factory;
+
+import devices.*;
+
+public class DeviceFactory {
+    public static Device createDevice(String type, int id) {
+        switch (type.toLowerCase()) {
+            case "light":
+                return new Light(id);
+            case "thermostat":
+                return new Thermostat(id);
+            case "door":
+                return new DoorLock(id);
+            default:
+                throw new IllegalArgumentException("Unknown device type: " + type);
+        }
+    }
+}
