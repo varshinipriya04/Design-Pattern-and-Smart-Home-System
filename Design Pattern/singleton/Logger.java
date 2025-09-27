ddc470db77ca7febed1f29889c1b41e03908149d
varshinipriya04 <varshini.sinju@gmail.com>
2025-09-27 12:57:20 +0530

    Create Logger.java

---
+++ Design Pattern/singleton/Logger.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger() { }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
